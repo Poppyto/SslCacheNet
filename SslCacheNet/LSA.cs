4da695f27120d9d15f17f769f4fed8b34275016d
Poppyto <poppyto@gmail.com>
2024-11-28 23:26:23 +0100

    feat: update version and hint

--- SslCacheNet/LSA.cs
+++ SslCacheNet/LSA.cs
@@ -206,7 +206,7 @@ namespace SslCacheNet
                 {
                     if (ntSubStatus == Native.NtSubStatus.STATUS_PRIVILEGE_NOT_HELD)
                     {
-                        throw new Win32Exception((unchecked((int)ntSubStatus)), "**** The TCB privilege is required to perform this operation.\n(hint: launch the command with PsExec from Sysinternals with admin rights: psexec.exe -s sslcache.exe -p)");
+                        throw new Win32Exception((unchecked((int)ntSubStatus)), "**** The TCB privilege is required to perform this operation.\n(hint: launch the command with PsExec from Sysinternals with admin rights: psexec.exe -s SslCacheNet.exe -p)");
                     }
                     else
                     {
